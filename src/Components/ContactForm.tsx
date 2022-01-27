1d0eca65a4c91893e790ba42fff339142f710f21
multipletwigs <starlaxone@gmail.com>
2022-01-26 17:27:43 -0800

    Added stuff

--- src/Components/ContactForm.tsx
+++ src/Components/ContactForm.tsx
@@ -1,13 +1,27 @@
-import { Box, Container, Flex } from "@chakra-ui/react"
+import { Box, Container, Flex, FormControl, FormLabel, Input, Textarea } from "@chakra-ui/react";
 import Navigation from "./Navigation";
 import ProfileCard from "./ProfileCard";
 
 const ContactForm = () => {
-    return (
-        <Box h="100vh">
-            Hi
-        </Box>
-    )
-}
+  return (
+    <Box
+      h="100vh"
+      display="flex"
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <FormControl w={["80%", "85%", '60%', "40%"]} gap="5" display="flex" flexDirection={'column'}>
 
-export default ContactForm; 
+        <Box>
+            <FormLabel htmlFor="first-name">Enter your name here</FormLabel>
+            <Input id="first-name" placeholder="Name" />
+        </Box>
+        
+        <Textarea placeholder='What do you think about this portfolio?' />
+
+      </FormControl>
+    </Box>
+  );
+};
+
+export default ContactForm;
