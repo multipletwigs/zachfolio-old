0bf326f00854b8a2134795876674c81ce7a2e230
multipletwigs <starlaxone@gmail.com>
2022-02-02 12:24:41 +0800

    Made ui improvements

--- src/Components/ProfileSectionTimeline.tsx
+++ src/Components/ProfileSectionTimeline.tsx
@@ -8,11 +8,11 @@ import { IoMdSchool } from 'react-icons/io'
 const ProfileSectionTimeline: () => JSX.Element = () => {
   return (
     <Flex justifyContent={"space-evenly"} mt="5">
-        <TimelineBox year="2001" icon={GiBabyBottle}></TimelineBox>
-        <TimelineBox year="2018" icon={IoMdSchool}></TimelineBox>
-        <TimelineBox year="2021" icon={GiPartyPopper}></TimelineBox>
-        <TimelineBox year="Now" icon={GiMaterialsScience}></TimelineBox>
-        <TimelineBox year="Future" icon={GiStairsGoal}></TimelineBox>
+        <TimelineBox year="2001" icon={GiBabyBottle} title="The year I was born 👶" description="Ready to face challenges from the world!"></TimelineBox>
+        <TimelineBox year="2018" icon={IoMdSchool} title="Graduated from 坤成中学 (KCHS) 🏫" description="Achived 10A's for SPM, with a class average of 71.5%"></TimelineBox>
+        <TimelineBox year="2021" icon={GiPartyPopper} title="Entered Year 2 of my Bachelors of CS in Data Science ✨" description="Experienced tremendous growth in programming skills, ending the semester with 3.91 CGPA and 85.125 WAM. Theory of Computation ruined my otherwise perfect GPA and WAM :("></TimelineBox>
+        <TimelineBox year="Now" icon={GiMaterialsScience} title="Year 2 Semester 2 💢" description="Falling in love with programming, learning everything I can. Currently focusing on backend development with Nest.js and Tensorflow/PyTorch for AI related projects."></TimelineBox>
+        <TimelineBox year="Future" icon={GiStairsGoal} title="Future?? 🔮" description="I see myself publishing many socially useful apps and working full-time either as an employee at my dream company, or a full-fledged freelancer building stuff I want to build. Join me on this journey to success :)"></TimelineBox>
     </Flex>
   );
 };
--- src/Components/TimelineBox.tsx
+++ src/Components/TimelineBox.tsx
@@ -1,28 +1,44 @@
-import { Box,  IconButton, Text } from "@chakra-ui/react"
+import {
+  Box,
+  IconButton,
+  Popover,
+  PopoverTrigger,
+  PopoverHeader,
+  PopoverContent, 
+  PopoverBody, 
+  Text,
+  PopoverArrow,
+  PopoverCloseButton,
+} from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { IconType } from "react-icons";
 
+const TimelineBox = (props: { year: string; icon: IconType, title: string, description: string }) => {
+  const IconMotion = motion(IconButton);
 
-const TimelineBox = (props: {
-    year: string,
-    icon: IconType
-}) => {
-    const IconMotion = motion(IconButton)
+  return (
+    <Box justifyContent="center" align="center">
+      <Popover placement="top">
+          <PopoverTrigger>
+                <IconMotion
+                whileHover={{ rotate: 360 }}
+                aria-label="Search database"
+                icon={<props.icon size={"25"} />}
+                rounded={"full"}
+                size="lg"/>
+          </PopoverTrigger>
+          <PopoverContent>
+    <PopoverArrow />
+    <PopoverCloseButton />
+    <PopoverHeader>{props.title}</PopoverHeader>
+    <PopoverBody>{props.description}</PopoverBody>
+  </PopoverContent>
+      </Popover>
+      <Text fontWeight={700} mt="2">
+        {props.year}
+      </Text>
+    </Box>
+  );
+};
 
-    return(
-        <Box justifyContent="center" align="center">
-            <IconMotion 
-            whileHover={{ rotate: 360 }}
-            aria-label='Search database' 
-            icon={<props.icon size={'25'}/>} 
-            rounded={'full'} 
-            size='lg'/> 
-            <Text 
-            fontWeight={700} 
-            mt="2" 
-            >{props.year}</Text>  
-        </Box>
-    )
-}
-
-export default TimelineBox; 
+export default TimelineBox;
